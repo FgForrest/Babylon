7ab61bcb15b68df4e755bdedd1e17b6ce744f740
Vladimír Kroupa <kroupa@fg.cz>
2020-11-09 15:47:36 +0100

    Add Kotlin dependency to write some tests in Kotlin. Add skeleton for first e2e test (cannot continue without further refactorings).

---
+++ src/test/java/com/fg/util/babylon/endtoend/export/ExportTest.kt
@@ -0,0 +1,23 @@
+package com.fg.util.babylon.endtoend.export
+
+import com.fg.util.babylon.entity.Arguments
+import com.fg.util.babylon.enums.Action
+import com.fg.util.babylon.processor.ExportProcessor
+import com.fg.util.babylon.service.GoogleSheetService
+
+class ExportTest {
+
+    fun `when translation data file is empty (first time export) then all localized strings are exported for translation`() {
+        val gss = GoogleSheetService()
+        val ep = ExportProcessor(gss)
+
+        val trSettings = Arguments()
+        trSettings.action = Action.EXPORT
+        trSettings.googleSheetId = "???"
+        trSettings.configFileName = "???"
+
+        ep.startTranslation(trSettings)
+
+    }
+
+}
