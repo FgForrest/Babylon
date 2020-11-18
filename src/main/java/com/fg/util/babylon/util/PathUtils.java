379056f7433c0377d38d25056babe49f2fa1f513
Vladimír Kroupa <kroupa@fg.cz>
2020-11-18 10:28:08 +0100

    Extract class from private method
    Need it accessible from other places.

---
+++ src/main/java/com/fg/util/babylon/util/PathUtils.java
@@ -0,0 +1,34 @@
+package com.fg.util.babylon.util;
+
+import com.fg.util.babylon.processor.AntPathResourceLoader;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathUtils {
+
+    /**
+     * Gets all relative paths of files for given path by org.springframework.core.io.support.PathMatchingResourcePatternResolver.
+     *
+     * @param path Path can contains masked expressions for by org.springframework.core.io.support.PathMatchingResourcePatternResolver.
+     * @return List of relative paths to files
+     * @throws IOException some exception derived from {@link IOException}
+     */
+    public List<String> expandPath(String path, AntPathResourceLoader resourceLoader) throws IOException {
+        Resource[] resources = resourceLoader.getResources("file:" + path);
+        List<String> list = new ArrayList<>();
+        String currentDir = System.getProperty("user.dir");
+        for (Resource resource : resources) {
+            String relativePath = resource.getFile().getPath();
+            // If is it absolute path then parse relative path to current directory.
+            if (relativePath.startsWith(currentDir)) {
+                relativePath = relativePath.substring(currentDir.length() + 1);
+            }
+            list.add(relativePath);
+        }
+        return list;
+    }
+
+}
