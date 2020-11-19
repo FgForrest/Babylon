fa19a662091b2fe18c7446a13fb458e1baf460eb
Vladimír Kroupa <kroupa@fg.cz>
2020-11-19 09:20:53 +0100

    Add test for case that is broken in old version

--- src/test/java/com/fg/util/babylon/export/MessageFileProcessorTest.kt
+++ src/test/java/com/fg/util/babylon/export/MessageFileProcessorTest.kt
@@ -9,41 +9,39 @@ class MessageFileProcessorTest {
     @Test
     fun `when no snapshot of previous translation and no translations (first run case) then all messages are exported to translation sheet`() {
         val emptySnapshot = FakeTranslationSnapshot(emptyMap())
-        val mfProcessor = MessageFileProcessor(emptySnapshot)
 
         val primaryMessages = mapOf(
                 "pagination.prev" to "Previous",
                 "pagination.next" to "Next",
-                "price.zeroPriceString" to "Free"
-        )
+                "price.zeroPriceString" to "Free")
+
         val translateTo = listOf("cz", "sk")
         val noTranslations = emptyMap<Language, Messages>()
 
         val expected = listOf(
                 listOf("pagination.prev", "Previous", null, null),
                 listOf("pagination.next", "Next", null, null),
-                listOf("price.zeroPriceString", "Free", null, null)
-        )
+                listOf("price.zeroPriceString", "Free", null, null))
+
+        val mfProcessor = MessageFileProcessor(emptySnapshot)
         val sheet = mfProcessor.prepareTranslationSheet("i18n/common.properties", primaryMessages, noTranslations, translateTo)
         assertEquals(expected, sheet)
     }
 
     @Test
-    fun `when there is a new primary message file then its messages are exported to translation sheet, but not the messages already in snapshot`() {
-        val snapshot = FakeTranslationSnapshot(mapOf(
-                "i18n/common.properties" to mapOf(
+    fun `when there is a new primary message file then its messages are exported to translation sheet, but the messages already in snapshot are not`() {
+        val snapshot = FakeTranslationSnapshot(
+                mapOf("i18n/common.properties" to mapOf(
                         "pagination.prev" to "Previous",
                         "pagination.next" to "Next",
                         "price.zeroPriceString" to "Free")))
-        val mfProcessor = MessageFileProcessor(snapshot)
 
         val primaryMessages = mapOf(
                 "pagination.prev" to "Previous",
                 "pagination.next" to "Next",
                 "price.zeroPriceString" to "Free",
                 "availability.text.ALWAYS_AVAILABLE" to "Always in stock",
-                "availability.text.NOT_AVAILABLE" to "Momentarily unavailable"
-        )
+                "availability.text.NOT_AVAILABLE" to "Momentarily unavailable")
 
         val translateTo = listOf("cz", "sk")
         val translations = mapOf(
@@ -54,14 +52,45 @@ class MessageFileProcessorTest {
                 "sk" to mapOf(
                         "pagination.prev" to "Predchádzajúce",
                         "pagination.next" to "Nasledujúce",
-                        "price.zeroPriceString" to "Zadarmo"
-                )
-        )
+                        "price.zeroPriceString" to "Zadarmo"))
 
         val expected = listOf(
                 listOf("availability.text.ALWAYS_AVAILABLE", "Always in stock", null, null),
-                listOf("availability.text.NOT_AVAILABLE", "Momentarily unavailable", null, null)
-        )
+                listOf("availability.text.NOT_AVAILABLE", "Momentarily unavailable", null, null))
+
+        val mfProcessor = MessageFileProcessor(snapshot)
+        val sheet = mfProcessor.prepareTranslationSheet("i18n/common.properties", primaryMessages, translations, translateTo)
+        assertEquals(expected, sheet)
+    }
+
+    @Test
+    fun `when a message is present in snapshot but is missing from primary message file then it is not exported to translation sheet`() {
+        val snapshot = FakeTranslationSnapshot(mapOf(
+                "i18n/common.properties" to mapOf(
+                        "pagination.prev" to "Previous",
+                        "pagination.next" to "Next",
+                        "price.zeroPriceString" to "Free")))
+
+        val primaryMessages = mapOf(
+                "pagination.prev" to "Previous",
+                "pagination.next" to "Next",
+                "availability.text.ALWAYS_AVAILABLE" to "Always in stock",
+                "availability.text.NOT_AVAILABLE" to "Momentarily unavailable")
+
+        val translateTo = listOf("cz", "sk")
+        val translations = mapOf(
+                "cz" to mapOf(
+                        "pagination.prev" to "Předchozí",
+                        "pagination.next" to "Následující"),
+                "sk" to mapOf(
+                        "pagination.prev" to "Predchádzajúce",
+                        "pagination.next" to "Nasledujúce"))
+
+        val expected = listOf(
+                listOf("availability.text.ALWAYS_AVAILABLE", "Always in stock", null, null),
+                listOf("availability.text.NOT_AVAILABLE", "Momentarily unavailable", null, null))
+
+        val mfProcessor = MessageFileProcessor(snapshot)
         val sheet = mfProcessor.prepareTranslationSheet("i18n/common.properties", primaryMessages, translations, translateTo)
         assertEquals(expected, sheet)
     }
