e544fc244666afd8b4eca5dfaceaeab59846959a
Vladimír Kroupa <kroupa@fg.cz>
2020-12-13 15:32:57 +0100

    Restructure ImportProcessor

--- src/main/java/com/fg/util/babylon/imp0rt/ImportProcessor.java
+++ src/main/java/com/fg/util/babylon/imp0rt/ImportProcessor.java
@@ -13,9 +13,6 @@ import com.fg.util.babylon.statistics.ImportFileStatistic;
 import com.fg.util.babylon.statistics.TranslationStatisticsOfImport;
 import com.fg.util.babylon.msgfile.TranslationFileUtils;
 import com.fg.util.babylon.util.JsonUtils;
-import com.google.api.services.sheets.v4.model.CellData;
-import com.google.api.services.sheets.v4.model.GridData;
-import com.google.api.services.sheets.v4.model.RowData;
 import com.google.api.services.sheets.v4.model.Sheet;
 import lombok.extern.apachecommons.CommonsLog;
 
@@ -23,7 +20,6 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.*;
-import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
 
 /**
@@ -37,38 +33,43 @@ public class ImportProcessor {
     private final PropertyFileLoader propertyFileLoader;
     private final LightGSheetService lightGSheetService;
 
-    private final String googleSheetId;
     private final TranslationConfiguration configuration;
 
     private final GitAdd gitAdd;
+    private final ImportSheetProcessor importSheetProcessor;
 
     public ImportProcessor(LightGSheetService lightGSheetService,
                            SnapshotManager snapshotManager,
                            PropertyFileLoader propertyFileLoader,
-                           String googleSheetId,
                            TranslationConfiguration configuration) {
         this.snapshotManager = snapshotManager;
         this.propertyFileLoader = propertyFileLoader;
         this.lightGSheetService = lightGSheetService;
-        this.googleSheetId = googleSheetId;
         this.configuration = configuration;
         this.gitAdd = new RuntimeExecGitAdd();
+        importSheetProcessor = new ImportSheetProcessor();
     }
 
-    public void doImport() throws IOException, GeneralSecurityException, InterruptedException {
+    public void doImport(String googleSheetId) throws IOException, GeneralSecurityException, InterruptedException {
         log.info("Started translation IMPORT with Google sheet id: '" + googleSheetId +"'");
         TranslationStatisticsOfImport statistics = new TranslationStatisticsOfImport();
         statistics.setAction(Action.IMPORT);
+
+        Snapshot snapshot = snapshotManager.getOrCreateDataFile();
         List<Sheet> sheets = lightGSheetService.listSheetsEagerly(googleSheetId);
         if (sheets == null || sheets.isEmpty()) {
             throw new IllegalArgumentException("Source spreadsheet " + googleSheetId + " not contains any sheets.");
         }
         // Using "for" loop to propagating of IOException.
         for (Sheet sheet : sheets) {
-            processSheet(sheet);
+            importSheetProcessor.processSheet(sheet, snapshot);
         }
-        saveTranslations(statistics);
-        saveDataFile();
+
+        Map<String, MessageFileContent> dataPropFiles = snapshotManager.getOrCreateDataFile().getDataPropFiles();
+        saveTranslations(statistics, configuration.getMutations(), dataPropFiles);
+
+        saveDataFile(snapshot, configuration.getDataFileName());
+
         log.info(statistics);
     }
 
@@ -76,132 +77,23 @@ public class ImportProcessor {
      * Saves BaseProcessor#dataFile with all set primary mutation properties values into file on disk.
      * @throws IOException some exception derived from {@link IOException}
      */
-    private void saveDataFile() throws IOException {
-        Snapshot snapshot = snapshotManager.getOrCreateDataFile();
+    private void saveDataFile(Snapshot snapshot, String dbFileName) throws IOException {
         if (!snapshot.getDataPropFiles().isEmpty()) {
-            JsonUtils.objToJsonFile(new File(configuration.getDataFileName()), snapshot, true);
+            JsonUtils.objToJsonFile(new File(dbFileName), snapshot, true);
         } else {
-            throw new IllegalArgumentException("Cannot save empty DataFile object to file \"" + configuration.getDataFileName() + "\"");
-        }
-    }
-
-    private void processSheet(Sheet sheet) throws IOException {
-        String sheetTitle = sheet.getProperties().getTitle();
-        log.info("Processing sheet \"" + sheetTitle + "\"...");
-        Integer fileId = parseFileIdFromSheetTitle(sheetTitle);
-        MessageFileContent propFile = getPropFileById(fileId);
-
-        List<GridData> sheetData = sheet.getData();
-        if (sheetData == null || sheetData.isEmpty()) {
-            log.warn("Sheet " + sheetTitle + " not contains any data in grid.");
-            return;
-        }
-        for (GridData gridData : sheetData) {
-            processGridDataOfSheet(sheetTitle, gridData, propFile);
-        }
-    }
-
-    private void processGridDataOfSheet(String sheetTitle, GridData gridData, MessageFileContent propFile) throws IOException {
-        List<RowData> rowsData = gridData.getRowData();
-        if (rowsData == null) {
-            log.warn("Sheet \"" + sheetTitle + " \" is empty");
-            return;
-        }
-        // First row is header
-        Map<Integer,String> header = createHeader(rowsData.get(0));
-        for (RowData rowData : rowsData.subList(1, rowsData.size())) {
-            if (rowData.getValues().stream().anyMatch(i-> i.getFormattedValue() != null && !Objects.equals(i.getFormattedValue(), "null"))){
-                processRowData(sheetTitle, header, rowData, propFile);
-            }
-        }
-    }
-
-    private Map<Integer,String> createHeader(RowData headerData) {
-        int col = 0;
-        Map<Integer,String> header = new LinkedHashMap<>();
-        for (CellData cellData : headerData.getValues()) {
-            String cellValue = cellData.getFormattedValue();
-            header.put(col++, cellValue);
-        }
-        return header;
-    }
-
-    /**
-     * Processes data of the one row in google sheet.
-     * @param sheetTitle Sheet title for target file identification
-     * @param header header values with column numbers as keys
-     * @param rowData data of the one row
-     * @throws IOException some exception derived from {@link IOException}
-     */
-    private void processRowData(String sheetTitle, Map<Integer,String> header, RowData rowData, MessageFileContent propFile) throws IOException {
-        int col = 0;
-        List<CellData> values = rowData.getValues();
-        String propKey = "";
-        for (CellData cellData : values) {
-            String colTitle = header.get(col);
-            String propValue = cellData.getFormattedValue();
-
-            // Double quotes in case of variable in property
-//            if (propValue != null && propValue.matches(".*\\{.}.*")){
-//                propValue = propValue.replace("'","''");
-//            }
-            if (col == 0) {
-                // First column contains properties keys.
-                propKey = propValue;
-            } else if (col == 1) {
-                // Second column contains text value of primary mutation.
-                propFile.putProperty(propKey, propValue);
-            } else {
-                // Next columns contains text values of secondary mutations. Column title contains name of the mutation.
-                String mutation = colTitle;
-                propFile.putMutationProperty(mutation, propKey, propValue);
-            }
-            col++;
-        }
-    }
-
-    /**
-     * Gets {@link MessageFileContent} object for one properties file by your unique id in json DataFile.
-     * @param fileId unique file ID
-     * @return Found {@link MessageFileContent} object or null if not found.
-     * @throws IOException some exception derived from {@link IOException}
-     */
-    private MessageFileContent getPropFileById(Integer fileId) throws IOException {
-        MessageFileContent propFile = snapshotManager.getOrCreateDataFile().getPropFileById(fileId);
-        if (propFile == null) {
-            String msg = "No record found by id=\"" + fileId + "\" in \"" + configuration.getDataFileName() + "\"";
-            throw new IllegalArgumentException(msg);
-        }
-        return propFile;
-    }
-
-    /**
-     * Parse unique id of the target properties file from sheet title (title format is "FileName" + "#" + "UniqueId")
-     * @param sheetTitle title of the sheet.
-     * @return File ID parsed from sheet title.
-     */
-    private Integer parseFileIdFromSheetTitle(String sheetTitle) {
-        try {
-            String[] split = sheetTitle.split("#");
-            if (split.length != 2) {
-                throw new IllegalArgumentException("Cannot parse unique id from string \"" + sheetTitle + "\"");
-            }
-            return Integer.parseInt(split[1]);
-        } catch (PatternSyntaxException | NumberFormatException e) {
-            throw new IllegalArgumentException(e.getMessage());
+            throw new IllegalArgumentException("Cannot save empty DataFile object to file \"" + dbFileName + "\"");
         }
     }
 
     /**
      * Saves all translated secondary mutations properties into target properties files.
      */
-    private void saveTranslations(TranslationStatisticsOfImport statistics) throws IOException, InterruptedException {
-        Map<String, MessageFileContent> dataPropFiles = snapshotManager.getOrCreateDataFile().getDataPropFiles();
+    private void saveTranslations(TranslationStatisticsOfImport statistics, List<String> translationLangs, Map<String, MessageFileContent> dataPropFiles) throws IOException, InterruptedException {
         for (Map.Entry<String, MessageFileContent> entry : dataPropFiles.entrySet()) {
             String primaryPropFilePath = entry.getKey();
             MessageFileContent messageFileContent = entry.getValue();
             // Save all translated properties into all mutation files defined by configuration.
-            for (String mutation : configuration.getMutations()) {
+            for (String mutation : translationLangs) {
                 saveMutationPropertiesToFile(primaryPropFilePath, mutation, messageFileContent, statistics);
             }
         }
@@ -290,8 +182,7 @@ public class ImportProcessor {
         savePropertiesToFile(updatedFileProps, mutationPropFilePath);
     }
 
-    private void savePropertiesToFile(PFActiveRecord propertyFileActiveRecord, String pathFileName) throws IOException, InterruptedException {
-
+    private void savePropertiesToFile(PFActiveRecord propertyFileActiveRecord, String pathFileName) throws IOException {
         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(pathFileName), StandardCharsets.UTF_8);
         propertyFileActiveRecord.save(outputStreamWriter);
 
